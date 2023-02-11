4715d6f4a9f37202b1a8449b3dbfb005b87898dd
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2023-02-11 01:01:55 -0800

    make it better
    changed the code to use gson bcuz gson is already used in minecraft

--- JsonUpdateChecker/UpdateChecker.java
+++ JsonUpdateChecker/UpdateChecker.java
@@ -2,67 +2,39 @@ package client.pkg;
 
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 public class UpdateChecker {
 
-	private static boolean available = false;
+	static String jsonFileUrl = "json url (for eg. https://github.com/BlueBloxKraft/MCP-Snippets/raw/main/JsonUpdateChecker/Example.json)";
+	public static double latest_version = -1;
 	
-	static String jsonUrl = "JSON URL goes here";
-	
-	public static boolean isAvailable() {
-		return available;
-	}
-	
-	public static void makeAvailable() {
-		available = true;
-	}
-	
-	public static void makeUnavailable() {
-		available = false;
-	}
-	
-	public static double getClientVersion() {
-		return 1.0;
-	}
-	
-	private static JsonArray getVersion(){
-        JsonArray latestVer = new JsonArray();
-        try{
-            URL url = new URL(jsonUrl);
-            InputStreamReader reader = new InputStreamReader(url.openStream());
-            latestVer = new Gson().fromJson(reader, JsonArray.class);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return latestVer;
-    }
-
-	private static List<Double> version(){
-        List<Double> ver = new ArrayList<>();
-        for(JsonElement version : getVersion()){
-            JsonObject e = version.getAsJsonObject();
-            ver.add(e.get("version").getAsDouble());
-        }
-        return ver;
-    }
-	
-	static double[] verArray = version().stream().mapToDouble(d -> d).toArray();
-	
-	public static void checkIfAvailable() {
-		if(verArray.length == 1) {
-			if(getClientVersion() < verArray[0]) {
-				makeAvailable();
-			} else {
-				makeUnavailable();
-			}
+	/*
+         * check the JSON file the latest version
+       	 */
+	public static void checkLatestVersion() {
+		try {
+			URL url = new URL(jsonFileUrl);
+			InputStreamReader isr = new InputStreamReader(url.openStream());
+			UpdateFile uf = new Gson().fromJson(isr, UpdateFile.class);
+			
+			latest_version = uf.version;
+			
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
+
+	/*
+	 * this boolean tells us whether an update has been found
+	 */ 
+	public static boolean foundUpdate() {
+		return latest_version > YourClient.instance.version;
+	}
+	
+	class UpdateFile {
+		double version;
 	}
 	
 }
